4de7ff67fc78b7f55e8cbd87e19de00f3b8e00fc
pro-stoi <123834383+pro-stoi@users.noreply.github.com>
2023-01-30 23:36:24 +0500

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
-function (){
 
-}
+function alll(){
+    var str=" ";
+    document.getElementById("demo").innerHTML=(document.getElementById('pole').value);
+    document.getElementById('pole').value=str;}
+    
