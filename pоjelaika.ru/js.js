840933c600e24936733e65880a8441080639eb84
pro-stoi <123834383+pro-stoi@users.noreply.github.com>
2023-02-02 22:38:59 +0500

    Add files via upload

---
+++ pоjelaika.ru/js.js
@@ -0,0 +1,12 @@
+var proverka = 0;//создали примитивную переменную
+function alll(){//вызываем функцию при нажатии кнопки
+    var str=(document.getElementById('pole').value);//инициализируем строковую переменную из поля ввода
+    document.getElementById('pole').value=" ";//вводим данные в поле ввода
+   
+proverka=proverka+1;//элементарный счетчик
+ var doc=document.createElement('div.fak');//создаем в переменную элемент
+doc.innerHTML=str+" "+proverka;//записываем текст + с счетчика в переменную с созданным элементом
+dem.appendChild(doc);//создаем элемент уже в самом html документе
+
+}
+    
